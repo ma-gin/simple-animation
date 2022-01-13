eb70bffa925a30d6751df201ede554b4030c997e
Matt Ginter <mattginter@gmail.com>
2022-01-13 16:56:17 +0100

    delete btn hover

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const deleteRow = (e) => {
     e.target.parentElement.parentElement.remove()
 }
+
+const showBtn = (e) => {
+    console.log(e.target)
+    const selectedRow = e.target.lastChild
+    console.log(selectedRow)
+    selectedRow.classList.toggle('show')
+}
